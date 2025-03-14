5cf09364ba2090d9dbbac9b180325fed5c546f17
timgaber <tim.gaber@scv.si>
2025-03-14 12:31:40 +0100

    kopirano od mušiča 1

---
+++ src/movies/entities/create-movie.dto.ts
@@ -0,0 +1,21 @@
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class CreateMovieDto {
+  @IsString()
+  title: string;
+  @IsString()
+  description: string;
+  @IsDateString()
+  release_date: Date;
+  @IsNumber()
+  @Min(1)
+  @Max(10)
+  rating: number;
+}
---
+++ src/users/entity/user.entity.ts
@@ -0,0 +1,39 @@
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { Movie } from '../../movies/entities/movie';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column({ unique: true })
+  email: string;
+  @Column()
+  password: string;
+  @Column()
+  firstName: string;
+  @Column()
+  lastName: string;
+  @Column({ nullable: true })
+  avatar?: string;
+  @CreateDateColumn()
+  createdAt: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => Movie, (movie) => movie.user)
+  movies: Movie[];
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+}
